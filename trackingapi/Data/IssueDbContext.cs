de72be07873d1773a50c5fe044a4152f0cc8d07e
Sudeep-mjn <sudeep.mjn@gmail.com>
2023-12-19 16:40:15 +0545

    first commit

---
+++ trackingapi/Data/IssueDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel;
+using trackingapi.Models;
+
+namespace trackingapi.Data
+{
+    public class IssueDbContext : DbContext
+    {
+        public IssueDbContext(DbContextOptions<IssueDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Issues> Issues { get; set; }
+    }
+
+}
+ 
---
+++ trackingapi/Models/Issues.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace trackingapi.Models
+{
+    public class Issues
+    {
+        public int id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Description { get; set; }
+        public Priority Priority { get; set; }
+        public IssuesType IssuesType { get; set; }
+        public DateTime? Completed { get; set; }
+    }
+
+    public enum Priority
+    {
+        Low, Medium,High
+    }
+
+    public enum IssuesType
+    {
+        Fearures, Bug, Documentation
+    }
+
+}
